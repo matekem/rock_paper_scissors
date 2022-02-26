57621da346567c42c10faf5cc9d277294d36d1a3
matekem <mate.kemenesi@gmail.com>
2022-02-26 12:16:24 +0100

    Added case insensivity and player prompt.

--- script.js
+++ script.js
@@ -1,42 +1,69 @@
-function computerPlay() 
-{
-    let plays = ["Rock" , "Paper" , "Scissors"];
-    let randomComputerPlay = plays[Math.floor(Math.random() * plays.length)];
-    console.log("Computer plays:"+ " " + randomComputerPlay);
+function computerPlay() {
+  let plays = ["Rock", "Paper", "Scissors"];
+  let randomComputerPlay = plays[Math.floor(Math.random() * plays.length)];
+  console.log("Computer plays:" + " " + randomComputerPlay);
 
-    return(randomComputerPlay);
+  return randomComputerPlay;
 }
 
-function playerPlay() 
-{
-    let plays = ["Rock" , "Paper" , "Scissors"];
-    let randomPlayerPlay = plays[Math.floor(Math.random() * plays.length)];
-    console.log("Player plays:" + " " + randomPlayerPlay);
-    
-    return(randomPlayerPlay);
+function playerPlay() {
+  let plays = ["Rock", "Paper", "Scissors"];
+  let randomPlayerPlay = plays[Math.floor(Math.random() * plays.length)];
+  console.log("Player plays:" + " " + randomPlayerPlay);
+
+  return randomPlayerPlay;
 }
 
-function oneRound(computerPlay, playerPlay)
-{
-    let paper = "Paper";
-    let rock = "Rock"
-    let scissors = "Scissors"
+function oneRound(computerPlay, playerPlay) {
+  let paper = "Paper";
+  let rock = "Rock";
+  let scissors = "Scissors";
 
-    //Draw scenario
-    if(playerPlay.toLowerCase() === computerPlay.toLowerCase()){
-        console.log("Draw!")
-    }
-    //Scenarios where the computer wins
-    else if(computerPlay === rock && playerPlay === scissors || computerPlay === scissors && playerPlay === paper || computerPlay === paper && playerPlay === rock){
-        console.log("Computer wins! " + computerPlay + " beats " + playerPlay + ".")
-    }
-    //Scenarios where the player wins
-    else if(computerPlay === rock && playerPlay === paper || computerPlay === scissors && playerPlay === rock || computerPlay === paper && playerPlay === scissors){
-        console.log("Player wins! " + playerPlay + " beats " + computerPlay + ".")
-    }
-    
+  //Draw scenario
+  if (playerPlay.toLowerCase() === computerPlay.toLowerCase()) {
+    console.log("Draw!");
+  }
+  //Scenarios where the computer wins
+  else if (
+    (computerPlay.toLowerCase() === rock.toLowerCase() &&
+      playerPlay.toLowerCase() === scissors.toLowerCase()) ||
+    (computerPlay.toLowerCase() === scissors.toLowerCase() &&
+      playerPlay.toLowerCase() === paper.toLowerCase()) ||
+    (computerPlay.toLowerCase() === paper.toLowerCase() &&
+      playerPlay.toLowerCase() === rock.toLowerCase())
+  ) {
+    console.log(
+      "You lose! " +
+        computerPlay.charAt(0).toUpperCase() +
+        computerPlay.slice(1) +
+        " beats " +
+        playerPlay.charAt(0).toUpperCase() +
+        playerPlay.slice(1) +
+        "."
+    );
+  }
+  //Scenarios where the player wins
+  else if (
+    (computerPlay.toLowerCase() === rock.toLowerCase() &&
+      playerPlay.toLowerCase() === paper.toLowerCase()) ||
+    (computerPlay.toLowerCase() === scissors.toLowerCase() &&
+      playerPlay.toLowerCase() === rock.toLowerCase()) ||
+    (computerPlay.toLowerCase() === paper.toLowerCase() &&
+      playerPlay.toLowerCase() === scissors.toLowerCase())
+  ) {
+    console.log(
+      "You win! " +
+        playerPlay.charAt(0).toUpperCase() +
+        playerPlay.slice(1) +
+        " beats " +
+        computerPlay.charAt(0).toUpperCase() +
+        computerPlay.slice(1) +
+        "."
+    );
+  }
 }
 
-let compRandom = computerPlay()/*.toLowerCase()*/;
-let playerRandom = playerPlay()/*.toLowerCase()*/;
-oneRound(compRandom, playerRandom);
+let compRandom = computerPlay(); /*.toLowerCase()*/
+let playerPrompt = window.prompt("Rock, paper, or scissors?");
+//let playerRandom = playerPlay()/*.toLowerCase()*/;
+oneRound(compRandom, playerPrompt);
