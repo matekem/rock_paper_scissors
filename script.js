812a1cdb5e5912bf55799a9493634e58e0d68f12
matekem <mate.kemenesi@gmail.com>
2022-04-06 13:42:57 +0200

    Cleaned up code

--- script.js
+++ script.js
@@ -16,33 +16,29 @@ const rockOff = document.querySelector("#rockBtn");
 const paperOff = document.querySelector("#paperBtn");
 const scissorsOff = document.querySelector("#scissorsBtn");
 const createRestartBtn = document.querySelector("#restartBtn");
-const cScoreText = document.querySelector("#c-score-text")
-const pScoreText = document.querySelector("#p-score-text")
-const playerScoreBox = document.querySelector(".playerScoreBox")
-const computerScoreBox = document.querySelector(".computerScoreBox")
-const computerImage = document.querySelector("#computer-image")
-const playerImage = document.querySelector("#player-image")
-const roundPlayBox = document.querySelector(".round-play-box")
-const roundCounter = document.querySelector("#round-counter")
-let roundCount=1
+const cScoreText = document.querySelector("#c-score-text");
+const pScoreText = document.querySelector("#p-score-text");
+const playerScoreBox = document.querySelector(".playerScoreBox");
+const computerScoreBox = document.querySelector(".computerScoreBox");
+const computerImage = document.querySelector("#computer-image");
+const playerImage = document.querySelector("#player-image");
+const roundPlayBox = document.querySelector(".round-play-box");
+const roundCounter = document.querySelector("#round-counter");
+let roundCount = 1;
 
 //Computer picks randomly from array: Rock, Paper or Scissors.
 function computerPlay() {
   let randomComputerPlay = plays[Math.floor(Math.random() * plays.length)];
-  
-  if(randomComputerPlay == "Rock"){
-    document.getElementById("computer-image").src="IMG/rock.png"
-  }
-  else if(randomComputerPlay == "Paper"){
-    document.getElementById("computer-image").src="IMG/paper.png"
-  }
-  else if(randomComputerPlay == "Scissors"){
-    document.getElementById("computer-image").src="IMG/scissors.png"
+
+  if (randomComputerPlay == "Rock") {
+    document.getElementById("computer-image").src = "IMG/rock.png";
+  } else if (randomComputerPlay == "Paper") {
+    document.getElementById("computer-image").src = "IMG/paper.png";
+  } else if (randomComputerPlay == "Scissors") {
+    document.getElementById("computer-image").src = "IMG/scissors.png";
   }
 
   return randomComputerPlay;
-
-
 }
 
 //Function to play one round of Rock-Paper-Scissors.
@@ -51,13 +47,12 @@ function playRound(computerPlay, playerPlay) {
   paper = "Paper";
   rock = "Rock";
   scissors = "Scissors";
-  
-  
-  roundCounter.textContent = `ROUND ${roundCount}`
+
+  roundCounter.textContent = `ROUND ${roundCount}`;
 
   //Draw scenario
   if (playerPlay.toLowerCase() === computerPlay.toLowerCase()) {
-    roundCount+=1;
+    roundCount += 1;
     resultsInside.textContent = "Draw!";
     resultsContainer.appendChild(resultsInside);
   }
@@ -70,7 +65,7 @@ function playRound(computerPlay, playerPlay) {
     (computerPlay.toLowerCase() === paper.toLowerCase() &&
       playerPlay.toLowerCase() === rock.toLowerCase())
   ) {
-    roundCount+=1;
+    roundCount += 1;
     resultsInside.textContent =
       "You lose! " +
       computerPlay.charAt(0).toUpperCase() +
@@ -80,7 +75,7 @@ function playRound(computerPlay, playerPlay) {
       playerPlay.slice(1) +
       ".";
     resultsContainer.appendChild(resultsInside);
-    
+
     compScore += 1;
 
     if (compScore == 5) {
@@ -98,7 +93,7 @@ function playRound(computerPlay, playerPlay) {
     (computerPlay.toLowerCase() === paper.toLowerCase() &&
       playerPlay.toLowerCase() === scissors.toLowerCase())
   ) {
-    roundCount+=1;
+    roundCount += 1;
     resultsInside.textContent =
       "You win! " +
       playerPlay.charAt(0).toUpperCase() +
@@ -117,67 +112,59 @@ function playRound(computerPlay, playerPlay) {
       gameOver();
     }
   }
-  
-  
 }
 
 //Button functionality
 
 function buttonFunctions() {
   rockBtn.addEventListener("click", () => {
-    
     playRound(computerPlay(), "rock");
     computerScoreDisplay();
     playerScoreDisplay();
-    
-    playerImage.setAttribute("style", "opacity:1")
-    document.getElementById("player-image").src="IMG/rock.png"
-    computerImage.setAttribute("style", "opacity:1")
+
+    playerImage.setAttribute("style", "opacity:1");
+    document.getElementById("player-image").src = "IMG/rock.png";
+    computerImage.setAttribute("style", "opacity:1");
   });
 
   paperBtn.addEventListener("click", () => {
     playRound(computerPlay(), "paper");
     computerScoreDisplay();
     playerScoreDisplay();
-    
-    playerImage.setAttribute("style", "opacity:1")
-    document.getElementById("player-image").src="IMG/paper.png"
-    computerImage.setAttribute("style", "opacity:1")
+
+    playerImage.setAttribute("style", "opacity:1");
+    document.getElementById("player-image").src = "IMG/paper.png";
+    computerImage.setAttribute("style", "opacity:1");
   });
 
   scissorsBtn.addEventListener("click", () => {
     playRound(computerPlay(), "scissors");
     computerScoreDisplay();
     playerScoreDisplay();
-    
-    playerImage.setAttribute("style", "opacity:1")
-    document.getElementById("player-image").src="IMG/scissors.png"
-    computerImage.setAttribute("style", "opacity:1")
-    
-    
-  });
 
-  
+    playerImage.setAttribute("style", "opacity:1");
+    document.getElementById("player-image").src = "IMG/scissors.png";
+    computerImage.setAttribute("style", "opacity:1");
+  });
 }
 
 //Display scores
 
 function computerScoreDisplay() {
-
   computerScoreActual.textContent = compScore;
-  cScoreText.textContent = "COMPUTER"
+  cScoreText.textContent = "COMPUTER";
 
   scoreboard.appendChild(computerScoreBox);
-  computerScoreBox.appendChild(cScoreText)
-  cScoreText.appendChild(computerScoreActual)
+  computerScoreBox.appendChild(cScoreText);
+  cScoreText.appendChild(computerScoreActual);
 }
 
 function playerScoreDisplay() {
-  playerScoreActual.textContent = playerScore
+  playerScoreActual.textContent = playerScore;
   pScoreText.textContent = "PLAYER";
-  
+
   scoreboard.appendChild(playerScoreBox);
-  playerScoreBox.appendChild(pScoreText)
+  playerScoreBox.appendChild(pScoreText);
   pScoreText.appendChild(playerScoreActual);
 }
 
@@ -188,8 +175,6 @@ function game() {
   compScore = 0;
 
   buttonFunctions();
-  
- 
 }
 
 //End game
@@ -202,8 +187,8 @@ function gameOver() {
   scissorsOff.disabled = true;
 
   createRestartBtn.innerHTML = "Rematch?";
-  
-  createRestartBtn.setAttribute("style", "opacity:1",);
+
+  createRestartBtn.setAttribute("style", "opacity:1");
   createRestartBtn.addEventListener("click", () => {
     restart();
   });
